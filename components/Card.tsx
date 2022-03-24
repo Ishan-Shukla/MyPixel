ff835ee26b31c6ade6233625a9c888ef15c3062d
Ishan Shukla <ishanshukla05@gmail.com>
2022-03-24 13:55:28 +0530

    Removed temporary comment

--- components/Card.tsx
+++ components/Card.tsx
@@ -62,22 +62,3 @@ const Card: React.FC<CardProps> = ({ post }) => {
 };
 
 export default Card;
-
-{
-  /* <Link href={`/post/${post.id}`}>
-<a>
-  <div className="relative m-2 mb-4">
-    {!loaded && (
-      <div className=" w-24 h-32 sm:w-32 sm:h-44 md:w-40 md:h-48 lg:w-44 lg:h-56 xl:w-44 2xl:w-52 2xl:h-64 animate-pulse rounded-lg bg-slate-400"></div>
-    )}
-    <img
-      src={post.imageUrl}
-      onLoad={() => setLoaded(true)}
-      loading="lazy"
-      alt={post.title}
-      className={`rounded-lg w-56 shadow-xl`}
-    />
-  </div>
-</a>
-</Link> */
-}
